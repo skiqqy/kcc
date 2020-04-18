e55031de24191fd102620f3bd33cf6cfddb8e25d
Stephen Cochrane <skippycochrane@gmail.com>
2020-04-18 20:31:33 +0200

    Updated test script
    [Details]
    N/A
    
    [Note]
    N/A
    
    [Bugs]
    N/A
    
    [File's changed]

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 ### This is used to run all tests for TravisCL ###
-echo -e "\nBeggining unit tests"
+### Please use `make test` for desired results  ###
+echo "Beggining unit tests"
 
 ### Test the scanner  ###
-echo -e "Testing scanner"
+echo "Testing scanner"
+for f in tests/scanner/*
+do
+	echo "./bin/testscanner $f"
+	./bin/testscanner $f
+done
 #########################
