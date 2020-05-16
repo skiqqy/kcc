c25a9767b41950acb4e4d6bd29f82dbc8ca96e48
Gregor Feierabend <23300469@sun.ac.za>
2020-05-16 20:45:45 +0200

    some better ideas :) ..

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -1,5 +1,35 @@
 #include "tokens.h"
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct {
+  struct token** st; //array of pointers to tokens
+  size_t all; //total allocated
+  size_t num; //number of used space by tokens
+} Grammar;
+
+void initGrammar(Grammar* g) {
+  MAIN.isTerm = TRUE;
+  MAIN.rep = "main";
+
+  g->st = (struct token**)malloc(16 * sizeof(struct token*));
+  g->st[0] = &MAIN;
+  g->num = 1;
+  g->all = 16;
+}
+
+void addToken(Grammar* g, struct token* t) {
+  if (g->num == g->all) {
+    g->all *= 2;
+    g->st = (struct token**)realloc(g->st, g->all * sizeof(struct token*));
+  }
+  g->st[g->num++] = t;
+}
 
 int main(){
+  Grammar ST;
+  
   return 0;
 }
