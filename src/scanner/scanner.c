09e621ad5e5eae4d6ad286201c05bfac3e44b26f
Gregor Feierabend <23300469@sun.ac.za>
2020-05-24 19:07:31 +0200

    added some regex functions; comments still missing

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -2,9 +2,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<regex.h>
 
 #define TRUE 1
 #define FALSE 0
+
+//THIS IS WORK IN PROGRESS, COMMENTS WILL HOPEFULLY FOLLOW SOON.
 
 typedef struct {
   struct token** st; //array of pointers to tokens
@@ -34,11 +37,69 @@ void addToken(Grammar* g, int isTerm, char* rep) {
   g->num++;
 }
 
-int main(){
+int r_comp (regex_t* r, const char* expr)
+{
+  int err_code = regcomp (r, expr, REG_EXTENDED);
+  if (err_code == 0) return 0;
+
+  char err_msg[512];
+  regerror (err_code, r, err_msg, 512);
+  printf ("error compiling regex '%s': %s\n", expr, err_msg);
+  return err_code;
+}
+
+int* r_gmatch (regex_t* r, const char* str)
+{
+  int mnum = 0;
+  int* matches;
+  matches = (int*)malloc(sizeof(int));
+
+  int gnum = r->re_nsub + 1;
+  regmatch_t *g = malloc(gnum * sizeof(regmatch_t));
+  regexec(r, str, (size_t) gnum, g, 0);
+
+  printf("len: %d >", (int)strlen(str));
+
+  int i;
+  for (i = 0; i < gnum; i++) {
+    if (g[i].rm_so == (size_t)(-1) || g[i].rm_so == g[i].rm_eo || g[i].rm_so > strlen(str)) {}
+    else {
+      printf("%d (%d:%d), ", (int) i, g[i].rm_so, g[i].rm_eo);
+      matches[mnum] = i;
+      mnum++;
+      matches = (int*)realloc(matches, (mnum+1) * sizeof(int));
+    }
+  }
+  printf("\n");
+  matches[0] = mnum;
+  return matches;
+}
+
+void tokenise(char* fname) {
+  regex_t r;
+  r_comp(&r, "(;)|(i)|(int)");
+
+  FILE *file;
+  char buffer[255];
+  file = fopen(fname, "r");
+  while(fscanf(file, "%s", buffer) == 1) {
+    printf(">>>%s\n", buffer);
+    int* gm = r_gmatch(&r, buffer);
+    if(gm[0] > 1) {
+      for(int i = 1; i < gm[0]; i++) {
+        printf("Matched token #%d\n", gm[i]);
+      }
+    }
+  }
+  fclose(file);
+  regfree(&r);
+}
+
+int main(int argv, char* argc[]){
   Grammar ST;
   initGrammar(&ST);
 
-  //reading built-in terminals from file
+  //reading built-in terminals from file (pre-sorted, just to simplify things for the moment)
   FILE *file;
   char buffer[255];
   file = fopen("term", "r");
@@ -50,6 +111,12 @@ int main(){
   for(int i=0; i<ST.num; i++){
     printf("%d: %s\n", i, ST.st[i]->rep);
   }
-  
+ 
+  if(argv != 2) {
+    printf("No arguments given.");
+  } else {
+    tokenise(argc[1]);
+  }
+
   return 0;
 }
